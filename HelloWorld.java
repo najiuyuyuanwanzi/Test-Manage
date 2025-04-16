dad3ee62a55767d606f8f80e4147f9545908f91a
LiMeng <1561990239@qq.com>
2025-04-16 20:07:48 +0800

    Updated HelloWorld message to Hello, Jenkins!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+// HelloWorld.java
+package com.example;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello, Jenkins!");
     }
 }
