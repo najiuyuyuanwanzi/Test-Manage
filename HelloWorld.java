dd23641fe28d6f2831fea35d577815d85c9f865d
najiuyuyuanwanzi <164744523+najiuyuyuanwanzi@users.noreply.github.com>
2025-04-17 13:29:01 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello,Jenkin!");
+        System.out.println("Hello,Jenkins!");
     }
 }
